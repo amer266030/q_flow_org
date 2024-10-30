b8ec93782605ea0a714650af5e23f3bb8dfd9d51
yara albouq <yaramalbouq@gmail.com>
2024-10-31 01:16:37 +0300

    modify home

---
+++ lib/reusable_components/cards/report_cards.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:q_flow_organizer/extensions/screen_size.dart';
+import 'package:q_flow_organizer/theme_data/extensions/text_style_ext.dart';
+import 'package:q_flow_organizer/theme_data/extensions/theme_ext.dart';
+
+class ReportCards extends StatelessWidget {
+  const ReportCards(
+      {super.key,
+      required this.onTap,
+      required this.title,
+      required this.icon});
+  final Function()? onTap;
+  final String title;
+  final IconData icon;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: context.screenWidth * 0.6,
+        width: context.screenWidth * 0.43,
+        child: Card(
+          elevation: 4,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24.0,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  icon,
+                  size: 60,
+                  color: context.primary,
+                ),
+                Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: context.bodyLarge.fontSize,
+                    fontWeight: FontWeight.w600,
+                    color: context.textColor1,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
